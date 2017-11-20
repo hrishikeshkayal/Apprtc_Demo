e68d3afb68723583691acb6bc9f59cc93e2795ca
Hrishikesh <hrishi.kayal@gmail.com>
2017-11-20 12:09:48 +0530

    Server port  has been updated

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ io.on('connection', function(socket) {
     });
 });
 
-http.listen(3000, function() {
-    console.log('listening on localhost:3000');
+http.listen(5000, function() {
+    console.log('listening on localhost:5000');
 });
